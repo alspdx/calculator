c4955332632dd2faf5cacfae71829b077bc1bb73
Adam Smith and Holly Wilkalis <holly@>
2017-10-16 09:56:34 -0700

    Create HTML and JS files for Calculator app, add script for addition function

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+number1 = parseInt(prompt("Enter a number: "));
+number2 = parseInt(prompt("Enter another number: "));
+
+alert(add(number1, number2));
