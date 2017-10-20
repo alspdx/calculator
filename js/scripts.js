81e40f18a20249bd3931cacd3033095776190e46
Adam <alspdx@gmail.com>
2017-10-19 22:01:57 -0700

    replace console logs with if else statement to make calculator functional

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,22 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-	$("form#calculator").submit(function(event) {
+	$("form#calculator").submit(function() {
 		event.preventDefault();
 		var number1 = parseFloat($("#input1").val());
 		var number2 = parseFloat($("#input2").val());
 		var operator = $("input:radio[name=operator]:checked").val();
-		console.log("1st number: " + number1);
-		console.log("2nd number: " + number2);
-		console.log("operator: " + operator);
-		var result = add(number1, number2);
+		var result;
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
+
 		$("#output").text(result);
 	});
 });
